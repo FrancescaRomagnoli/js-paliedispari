e9fa5fd6b2777899ac707ce5d815fe885e84e8b1
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-10-18 16:33:18 +0200

    add first exc function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 // $ Palidroma
 // $ Chiedere all’utente di inserire una parola
 // $ Creare una funzione per capire se la parola inserita è palindroma
+
+// # functions
+
+function wordReverse(word) {
+  let newWord = "";
+
+  for (let i = word.length - 1; i >= 0; i--) {
+    newWord += word[i];
+  }
+
+  return newWord;
+}
+
+// # user data
+
+const userWord = prompt("Inserisci una parola");
+const isUserWordValid = userWord.length > 0;
+console.log(userWord, isUserWordValid);
+
+let reversedWord = wordReverse("hola");
+console.log(reversedWord);
